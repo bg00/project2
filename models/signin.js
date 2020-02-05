437ab8b19d616e73d20ae27d316159d0b3465fda
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-02-05 11:40:00 -0600

    Remove deprecated files

--- models/signin.js
+++
@@ -1,9 +0,0 @@
-// Create a sequelize for signin form
-
-module.exports = function(sequelize, DataTypes) {
-  var Model = sequelize.define("loginDB", {
-    username : DataTypes.STRING,
-    password : DataTypes.STRING,
-  }); 
-  return Model;
-};
